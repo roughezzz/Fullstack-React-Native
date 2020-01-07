9bd08640c4c6482df2d5f67fa448fbc9c2d001b6
abir-at-198938739785 <abir-at-198938739785>
2020-01-07 20:47:07 +0600

    Image Feed completed

--- image-feed/App.js
+++ image-feed/App.js
@@ -1,18 +1,78 @@
 import React, { Component } from "react";
-import { StyleSheet, Platform, View } from "react-native";
+import { StyleSheet, Platform, View, Modal, AsyncStorage } from "react-native";
 import Constants from "expo-constants";
 
 import Feed from "./screens/Feed";
+import Comments from "./screens/Comments";
+
+const ASYNC_STORAGE_COMMENTS_KEY = "ASYNC_STORAGE_COMMENTS_KEY";
 
 export default class App extends Component {
+  state = {
+    commentsForItem: {},
+    showModal: false,
+    selectedItemId: null
+  };
+
+  async componentDidMount() {
+    try {
+      const commentsForItem = await AsyncStorage.getItem(
+        ASYNC_STORAGE_COMMENTS_KEY
+      );
+      this.setState({
+        commentsForItem: commentsForItem ? JSON.parse(commentsForItem) : {}
+      });
+    } catch (e) {
+      console.log("ERROR: ", e);
+    }
+  }
+
+  openCommentScreen = id => {
+    this.setState({ showModal: true, selectedItemId: id });
+  };
+
+  closeCommentScreen = () => {
+    this.setState({ showModal: false, selectedItemId: null });
+  };
+
+  onSubmitComment = text => {
+    const { selectedItemId, commentsForItem } = this.state;
+    const comments = commentsForItem[selectedItemId] || [];
+    const updated = {
+      ...commentsForItem,
+      [selectedItemId]: [...comments, text]
+    };
+
+    this.setState({ commentsForItem: updated });
+
+    try {
+      AsyncStorage.setItem(ASYNC_STORAGE_COMMENTS_KEY, JSON.stringify(updated));
+    } catch (e) {
+      console.log("ERROR: ", e);
+    }
+  };
+
   render() {
-    const items = [
-      { id: 0, author: "Bob Ross" },
-      { id: 1, author: "Chuck Norris" }
-    ];
+    const { commentsForItem, showModal, selectedItemId } = this.state;
     return (
       <View style={styles.container}>
-        <Feed style={styles.feed} />
+        <Feed
+          style={styles.feed}
+          commentsForItem={commentsForItem}
+          onPressComments={this.openCommentScreen}
+        />
+        <Modal
+          visible={showModal}
+          animationType="slide"
+          onRequestClose={this.closeCommentScreen}
+        >
+          <Comments
+            style={styles.comments}
+            comments={commentsForItem[selectedItemId] || []}
+            onClose={this.closeCommentScreen}
+            onSubmitComment={this.onSubmitComment}
+          />
+        </Modal>
       </View>
     );
   }
@@ -31,6 +91,13 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop:
       Platform.OS === "android" || platformVersion < 11
+        ? Constants.statusBarHeight
+        : 0
+  },
+  comments: {
+    flex: 1,
+    marginTop:
+      Platform.OS === "ios" && platformVersion < 11
         ? Constants.statusBarHeight
         : 0
   }
--- image-feed/screens/Feed.js
+++ image-feed/screens/Feed.js
@@ -11,7 +11,10 @@ import CardList from "../components/CardList";
 
 export default class Feed extends React.Component {
   static propTypes = {
-    style: ViewPropTypes.style
+    style: ViewPropTypes.style,
+    commentsForItem: PropTypes.objectOf(PropTypes.arrayOf(PropTypes.string))
+      .isRequired,
+    onPressComments: PropTypes.func.isRequired
   };
 
   static defaultProps = {
@@ -40,7 +43,7 @@ export default class Feed extends React.Component {
   }
 
   render() {
-    const { style } = this.props;
+    const { style, commentsForItem, onPressComments } = this.props;
     const { loading, error, items } = this.state;
     if (loading) {
       return <ActivityIndicator size="large" />;
@@ -50,7 +53,11 @@ export default class Feed extends React.Component {
     }
     return (
       <SafeAreaView style={style}>
-        <CardList items={items} />
+        <CardList
+          items={items}
+          commentsForItem={commentsForItem}
+          onPressComments={onPressComments}
+        />
       </SafeAreaView>
     );
   }
